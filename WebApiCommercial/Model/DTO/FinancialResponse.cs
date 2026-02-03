03c59b3856855de5f4ce90cab767fb2274d41aed
vinic <vinic@VINICIUDEV>
2026-02-03 12:09:22 -0300

    novas formas de pagamento financeiro.

--- WebApiCommercial/Model/DTO/FinancialResponse.cs
+++ WebApiCommercial/Model/DTO/FinancialResponse.cs
@@ -7,28 +7,15 @@ namespace Model.DTO
     {
         public decimal Value { get; set; }
         public int Id { get; set; }
-
-
         public FinancialStatus FinancialStatus { get; set; }
-
-
         public FinancialType FinancialType { get; set; }
-
-
         public OriginFinancial Origin { get; set; }
-
-
         public int? IdSale { get; set; }
-
-
         public DateTime CreationDate { get; set; }
-
-
         public DateTime DueDate { get; set; }
-
-
         public string Description { get; set; }
-        public string PaymentType { get; set; }
+        public string? PaymentMethodName { get; set; }
+        public int? PaymentMethodId { get; set; }
         public int IdCompany { get; set; }
         public string ?ClientName { get; set; }
         public int? ClientId { get; set; }
