8e6e171fa096285ff7cc18a4a3cb9938799f5362
vinic <vinic@VINICIUDEV>
2026-04-07 10:29:32 -0300

    .

---
+++ WebApiCommercial/Repository/FinancialPaymentMethodRepository.cs
@@ -0,0 +1,15 @@
+﻿using Model.Moves;
+
+namespace Repository
+{
+
+	public class FinancialPaymentMethodRepository : GenericRepository<FinancialPaymentMethod>, IFinancialPaymentMethodRepository
+	{
+		public FinancialPaymentMethodRepository(ContextBase dbContext) : base(dbContext)
+		{
+		}
+	}
+	public interface IFinancialPaymentMethodRepository : IGenericRepository<FinancialPaymentMethod>
+	{
+	}
+}
