f3e6380294bff316bd96fcc94aeb14f25238b9e8
vinic <vinic@VINICIUDEV>
2026-07-03 09:46:43 -0300

    .

--- WebApiCommercial/Service/NFeService.cs
+++ WebApiCommercial/Service/NFeService.cs
@@ -1010,7 +1010,7 @@ namespace Service
 					ICMS = new ICMS { TipoICMS = tipoICMS },
 					COFINS = calculador.CalcularCOFINS(),
 					PIS = calculador.CalcularPIS(),
-					IBSCBS = calculador.CalcularIBSCBS()
+					//IBSCBS = calculador.CalcularIBSCBS()
 				}
 			};
 
@@ -1573,36 +1573,36 @@ namespace Service
 				decimal totalCBS = produtos.Sum(p =>
 					p.imposto?.IBSCBS?.gIBSCBS?.gCBS?.vCBS ?? 0);
 
-				t.IBSCBSTot = new IBSCBSTot
-				{
-					vBCIBSCBS = totalBCIBSCBS,
-					gIBS = new gIBSTotal
-					{
-						vIBS = Math.Round(totalIBS, 2),
-						gIBSUF = new gIBSUFTotal
-						{
-							vIBSUF = Math.Round(totalIBSUF, 2),
-							vDif = 0,
-							vDevTrib = 0
-						},
-						gIBSMun = new gIBSMunTotal
-						{
-							vIBSMun = Math.Round(totalIBSMun, 2),
-							vDif = 0,
-							vDevTrib = 0
-						},
-						vCredPres = 0,
-						vCredPresCondSus = 0
-					},
-					gCBS = new gCBSTotal
-					{
-						vCBS = Math.Round(totalCBS, 2),
-						vDif = 0,
-						vDevTrib = 0,
-						vCredPres = 0,
-						vCredPresCondSus = 0
-					}
-				};
+				//t.IBSCBSTot = new IBSCBSTot
+				//{
+				//	vBCIBSCBS = totalBCIBSCBS,
+				//	gIBS = new gIBSTotal
+				//	{
+				//		vIBS = Math.Round(totalIBS, 2),
+				//		gIBSUF = new gIBSUFTotal
+				//		{
+				//			vIBSUF = Math.Round(totalIBSUF, 2),
+				//			vDif = 0,
+				//			vDevTrib = 0
+				//		},
+				//		gIBSMun = new gIBSMunTotal
+				//		{
+				//			vIBSMun = Math.Round(totalIBSMun, 2),
+				//			vDif = 0,
+				//			vDevTrib = 0
+				//		},
+				//		vCredPres = 0,
+				//		vCredPresCondSus = 0
+				//	},
+				//	gCBS = new gCBSTotal
+				//	{
+				//		vCBS = Math.Round(totalCBS, 2),
+				//		vDif = 0,
+				//		vDevTrib = 0,
+				//		vCredPres = 0,
+				//		vCredPresCondSus = 0
+				//	}
+				//};
 			}
 
 			return t;
