e2bb0cf98bd5ce2c5084bff020ed184543140f01
vinic <vinic@VINICIUDEV>
2026-07-03 14:45:13 -0300

    ajuste capac colunas nfeemission

---
+++ WebApiCommercial/Repository/Migrations/20260703174406_aumentar-cap-colunas-nfeemission.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class aumentarcapcolunasnfeemission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ErrorMessage",
+                table: "tb_nfeEmission",
+                type: "text",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ErrorMessage",
+                table: "tb_nfeEmission",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+    }
+}
